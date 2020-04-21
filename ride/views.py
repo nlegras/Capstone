4e1b97d80b4e4ce87d03528188f7cf2fc1b89de3
Daniel Carter <dcarter18@mail.wou.edu>
2020-04-20 17:31:35 -0700

    Added functionality to reserve a ride

--- ride/views.py
+++ ride/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from submit.models import Rides
 from django.utils import timezone
+from .forms import reserveForm
 # Create your views here.
 
 def post_rides(request):
    rides = Rides.objects.all()
    return render(request, 'rides.html', {'rides': rides})
+
+def resRide(request):
+   if request.method == "POST":
+      resForm = reserveForm(request.POST)
+      if resForm.is_valid():
+         reserveID = request.POST.get('reserveID')
+         reservation = Rides.objects.get(id=reserveID)
+         reservation.reserved = 'Reserved'
+         reservation.save()
+
+   return HttpResponseRedirect('.')
 
