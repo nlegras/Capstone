2679d51f74b2865c534b2ac948d7cd306e469207
nlegras <nlegras14@wou.edu>
2020-05-07 20:04:34 +0000

    enter city and state in place of zip

--- submit/forms.py
+++ submit/forms.py
@@ -1,13 +1,30 @@
 from django import forms
+from pyzipcode import ZipCodeDatabase
+zcdb = ZipCodeDatabase()
+
+
 
 class rideForm(forms.Form):
     departDate = forms.DateField()
     departTime = forms.TimeField()
     seatCapacity = forms.IntegerField()
-    departZip = forms.IntegerField()
-    arrivalZip = forms.IntegerField()
+    departCity = forms.CharField(required=False)
+    departState = forms.CharField(required=False)    
+    departZip = forms.CharField(required=False)
+    arrivalZip = forms.CharField()
     driverEmail = forms.EmailField()
     riderPrice = forms.DecimalField(max_digits=5, decimal_places=2)
     drisSmokes = forms.CheckboxSelectMultiple()
     ridersPets = forms.CheckboxSelectMultiple()
     ridersLugg = forms.IntegerField()
+    print('hello')
+    
+    def clean(self):
+        departCity = self.cleaned_data.get('departCity')
+        departState = self.cleaned_data.get('departState')
+        departZip   = zcdb.find_zip(city=departCity, state=departState)[0].zip
+        print(departZip)
+        if not (departCity and departState) and not departZip:
+            raise forms.ValidationError('Enter city and state or zip')
+        return self.cleaned_data
+
--- submit/views.py
+++ submit/views.py
@@ -2,20 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import rideForm
 from .models import Rides
+from pyzipcode import ZipCodeDatabase
+zcdb = ZipCodeDatabase()
 # Create your views here.
 
 def index(request):
     if request.method == 'POST':
         form = rideForm(request.POST)
         if form.is_valid():
-            departDate = request.POST.get('departDate')
-            departTime = request.POST.get('departTime')
-            departZip = request.POST.get('departZip')
-            arrivalZip = request.POST.get('arrivalZip')
+            departDate  = request.POST.get('departDate')
+            departTime  = request.POST.get('departTime')
+            # departZip = request.POST.get('departZip')
+            # departCity  = request.POST.get('departCity')
+            # departState = request.POST.get('departState')
+            departZip   = zcdb.find_zip(city=request.POST.get('departCity'), state=request.POST.get('departState'))[0].zip
+            print(departZip)
+            arrivalZip  = request.POST.get('arrivalZip')
+            # arrivalCity  = request.POST.get('aCity')
+            # arrivalState = request.POST.get('aState')
+            # arrivalZip   = zcdb.find_zip(city=arrivalCity, state=arrivalState)[0].zip          
             driverEmail = request.POST.get('driverEmail')
-            rideLugg = request.POST.get('ridersLugg')
-            seatCapac = request.POST.get('seatCapacity')
-            riderPrice = request.POST.get('riderPrice')
+            rideLugg    = request.POST.get('ridersLugg')
+            seatCapac   = request.POST.get('seatCapacity')
+            riderPrice  = request.POST.get('riderPrice')
             if request.POST.get('drisSmokes') == 'on':
                 driverSmokes = 1
             else:
@@ -24,9 +33,11 @@ def index(request):
                 ridePets = 1
             else:
                 ridePets = 0
+            print(form.cleaned_data)
             record = Rides(depDate=departDate, depTime=departTime, depZip=departZip, arrZip=arrivalZip, driEmail=driverEmail, seatCapacity=seatCapac, reserved='Open', driSmokes=driverSmokes, riderPets=ridePets, riderLugg=rideLugg, riderPrice=riderPrice)
             record.save()
         else:
             pass
+            print(form.errors)
             
     return render(request, 'rideSubmit.html')
