5db669a37f8842e7a7726d594a370a9717c6b37b
nlegras <nlegras@wou.edu>
2020-05-08 02:54:38 +0000

    arrival city and state added

--- submit/forms.py
+++ submit/forms.py
@@ -11,7 +11,9 @@ class rideForm(forms.Form):
     departCity = forms.CharField(required=False)
     departState = forms.CharField(required=False)    
     departZip = forms.CharField(required=False)
-    arrivalZip = forms.CharField()
+    arrivalCity = forms.CharField(required=False)
+    arrivalState = forms.CharField(required=False)  
+    arrivalZip = forms.CharField(required=False)
     driverEmail = forms.EmailField()
     riderPrice = forms.DecimalField(max_digits=5, decimal_places=2)
     drisSmokes = forms.CheckboxSelectMultiple()
@@ -19,7 +21,7 @@ class rideForm(forms.Form):
     ridersLugg = forms.IntegerField()
     print('hello')
     
-    def clean(self):
+    def clean_departZip(self):
         cleaned_data = super().clean()
         departCity = cleaned_data.get('departCity')
         departState = cleaned_data.get('departState')
@@ -33,5 +35,21 @@ class rideForm(forms.Form):
                     self.add_error('departCity', 'no city')
                 if not departState:
                     self.add_error('departState', 'no state')
+        return self.cleaned_data
+        
+    def clean_arrivalZip(self):
+        cleaned_data = super().clean()
+        arrivalCity = cleaned_data.get('arrivalCity')
+        arrivalState = cleaned_data.get('arrivalState')
+        arrivalZip = cleaned_data.get('arrivalZip')
+        
+        if not arrivalZip:
+            if arrivalCity and arrivalState:
+                arrivalZip = zcdb.find_zip(city=arrivalCity, state=arrivalState)[0].zip
+            else:
+                if not arrivalCity:
+                    self.add_error('arrivalCity', 'no city')
+                if not arrivalState:
+                    self.add_error('arrivalState', 'no state')
         return self.cleaned_data
 
--- submit/views.py
+++ submit/views.py
@@ -17,13 +17,11 @@ def index(request):
             departZip = request.POST.get('departZip')
             if not departZip:
                 departZip = zcdb.find_zip(city=departCity, state=departState)[0].zip
-            print("check: ", departZip)
-            # departZip   = zcdb.find_zip(city=request.POST.get('departCity'), state=request.POST.get('departState'))[0].zip
-            # print(departZip)
+            arrivalCity  = request.POST.get('arrivalCity')
+            arrivalState = request.POST.get('arrivalState')
             arrivalZip  = request.POST.get('arrivalZip')
-            # arrivalCity  = request.POST.get('aCity')
-            # arrivalState = request.POST.get('aState')
-            # arrivalZip   = zcdb.find_zip(city=arrivalCity, state=arrivalState)[0].zip          
+            if not arrivalZip:
+                arrivalZip = zcdb.find_zip(city=arrivalCity, state=arrivalState)[0].zip
             driverEmail = request.POST.get('driverEmail')
             rideLugg    = request.POST.get('ridersLugg')
             seatCapac   = request.POST.get('seatCapacity')
@@ -36,10 +34,7 @@ def index(request):
                 ridePets = 1
             else:
                 ridePets = 0
-            print(form)
-            print(form.cleaned_data)
             record = Rides(depDate=departDate, depTime=departTime, depZip=departZip, arrZip=arrivalZip, driEmail=driverEmail, seatCapacity=seatCapac, reserved='Open', driSmokes=driverSmokes, riderPets=ridePets, riderLugg=rideLugg, riderPrice=riderPrice)
-            print(record.depZip) 
             record.save()
         else:
             pass
